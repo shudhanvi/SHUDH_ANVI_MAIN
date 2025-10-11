014cfbc20b17f3f063c44c374bbfa6a4d126213f
sandeepreddy2429 <jereddysandeep24@gmail.com>
2025-10-11 16:36:18 +0530

    minor changes sturday sandeep

--- src/Components/WardDetailsPopUp.jsx
+++ src/Components/WardDetailsPopUp.jsx
@@ -17,6 +17,7 @@ const WardDetailsPopUp = ({ selectedWard, setSelectedWard, wardData }) => {
     perimeter,
     ward_id,
     ward_name,
+    Division,
     waste_colleccted,
   } = selectedWardInfo;
 
@@ -33,7 +34,7 @@ const WardDetailsPopUp = ({ selectedWard, setSelectedWard, wardData }) => {
       >
         <div className="flex flex-col justify-center relative align-middle gap-2 text-white text-left">
           <h1 className="text-xl font-bold ">{`Ward: ${ward_name}`}</h1>
-          <p className="text-[12px]">Division : Kukatpally</p>
+          <p className="text-[12px]">{`Division :${Division}`} </p>
           <button
             type="button"
             className="btn-hover cursor-pointer"
